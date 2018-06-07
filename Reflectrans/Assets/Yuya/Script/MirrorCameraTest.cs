eb3739010a42dbb332db4048f6a47705416045e2
c17016 <c17016@std.it-college.ac.jp>
2018-06-07 15:46:05 +0900

    作業開始

---
+++ Reflectrans/Assets/Yuya/Script/MirrorCameraTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorCameraTest : MonoBehaviour
+{
+    public Transform player;
+    public Transform mirrorCamera;
+    private Vector3 lookTarget;
+    private Vector3 newLook;
+    private float dist;
+
+    void Start()
+    {
+        //   lookTarget = mirrorCamera.position + new Vector3(0, 0, 4); 
+        lookTarget = mirrorCamera.position + (mirrorCamera.right);
+        newLook = lookTarget;
+    }
+    void Update()
+    {
+        newLook.x = lookTarget.x - (player.position.x - mirrorCamera.position.x);
+        mirrorCamera.LookAt(newLook, Vector3.up);
+
+        dist = Vector3.Distance(player.transform.position, mirrorCamera.transform.position);
+        mirrorCamera.GetComponent<Camera>().fieldOfView = 80 - (dist * 2);
+    }
+
+
+
+
+    //float diffX = toX - fromX;
+    //float diffY = toY - fromY;
+
+    //float radian = Mathf.Atan2(diffY, diffX);
+    //float degree = radian * Mathf.Rad2Deg;
+    //lineTransform.eulerAngles = new Vector3(0, 0, degree);         
+
+
+
+
+
+    //public Transform target;    
+
+    //void Update()
+    //{
+    //    Vector3 targetDir = target.position - transform.position;
+    //    float angle = Vector3.Angle(targetDir, transform.forward);
+
+    //    //if (angle < 5.0f)
+    //    //    print("close");
+
+    //    transform.rotation = Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z);
+    //}
+
+
+
+
+    //public Transform player;
+    //public Transform mirrorCamera;
+    //private Vector3 lookTarget;
+    //private Vector3 newLook;
+    //private float dist;
+
+    //void Start()
+    //{
+    //    //   lookTarget = mirrorCamera.position + new Vector3(0, 0, 4); 
+    //    lookTarget = mirrorCamera.position + (4 * mirrorCamera.right);
+    //    newLook = lookTarget;
+    //}
+    //void Update()
+    //{
+    //    newLook.x = lookTarget.x - (player.position.x - mirrorCamera.position.x);
+    //    mirrorCamera.LookAt(newLook, Vector3.up);
+
+    //    dist = Vector3.Distance(player.transform.position, mirrorCamera.transform.position);
+    //    mirrorCamera.GetComponent<Camera>().fieldOfView = 80 - (dist * 2);
+    //}
+}
---
+++ Reflectrans/Assets/Yuya/Script/mirrorTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirrorTest : MonoBehaviour
+{
+    public Transform targetPlayer;
+    public GameObject playerDirection;
+    public GameObject cameraDirection;
+
+    void Update()
+    {
+        playerDirection.transform.LookAt(targetPlayer);
+        print(playerDirection.transform.rotation);
+
+        cameraDirection.transform.rotation = new Quaternion(
+            0, playerDirection.transform.rotation.y * -1, 0, playerDirection.transform.rotation.w);
+
+        //playerDirection.transform.rotation = new Quaternion(
+        //    0, Mathf.Clamp(playerDirection.transform.rotation.y, -90, 90), 0, playerDirection.transform.rotation.w);
+
+
+        //var aim = this.targetPlayer.transform.position - transform.position;
+        //var look = Quaternion.LookRotation(aim);
+        //playerDirecton.transform.localRotation = look; 
+    }
+
+
+}
