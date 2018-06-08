3d8340308552149400ee4a38630462a1ebe49dd9
c17016 <c17016@std.it-college.ac.jp>
2018-06-08 16:40:56 +0900

    鏡とりあえず完成

--- Reflectrans/Assets/Yuya/Script/MirrorCameraTest.cs
+++ Reflectrans/Assets/Yuya/Script/MirrorCameraTest.cs
@@ -4,75 +4,21 @@ using UnityEngine;
 
 public class MirrorCameraTest : MonoBehaviour
 {
-    public Transform player;
-    public Transform mirrorCamera;
-    private Vector3 lookTarget;
-    private Vector3 newLook;
-    private float dist;
+    public Transform targetPlayer;
+    public GameObject playerDirection;
+    public GameObject cameraDirection;
 
-    void Start()
-    {
-        //   lookTarget = mirrorCamera.position + new Vector3(0, 0, 4); 
-        lookTarget = mirrorCamera.position + (mirrorCamera.right);
-        newLook = lookTarget;
-    }
     void Update()
     {
-        newLook.x = lookTarget.x - (player.position.x - mirrorCamera.position.x);
-        mirrorCamera.LookAt(newLook, Vector3.up);
+        playerDirection.transform.LookAt(targetPlayer);
+        //print(playerDirection.transform.rotation);
 
-        dist = Vector3.Distance(player.transform.position, mirrorCamera.transform.position);
-        mirrorCamera.GetComponent<Camera>().fieldOfView = 80 - (dist * 2);
+        cameraDirection.transform.rotation = new Quaternion(
+            0, playerDirection.transform.rotation.y * -1, 0, playerDirection.transform.rotation.w);
+
+        playerDirection.transform.rotation = new Quaternion(
+            0, Mathf.Clamp(playerDirection.transform.rotation.y, -90, 90), 0, playerDirection.transform.rotation.w);
+
     }
 
-
-
-
-    //float diffX = toX - fromX;
-    //float diffY = toY - fromY;
-
-    //float radian = Mathf.Atan2(diffY, diffX);
-    //float degree = radian * Mathf.Rad2Deg;
-    //lineTransform.eulerAngles = new Vector3(0, 0, degree);         
-
-
-
-
-
-    //public Transform target;    
-
-    //void Update()
-    //{
-    //    Vector3 targetDir = target.position - transform.position;
-    //    float angle = Vector3.Angle(targetDir, transform.forward);
-
-    //    //if (angle < 5.0f)
-    //    //    print("close");
-
-    //    transform.rotation = Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z);
-    //}
-
-
-
-
-    //public Transform player;
-    //public Transform mirrorCamera;
-    //private Vector3 lookTarget;
-    //private Vector3 newLook;
-    //private float dist;
-
-    //void Start()
-    //{
-    //    //   lookTarget = mirrorCamera.position + new Vector3(0, 0, 4); 
-    //    lookTarget = mirrorCamera.position + (4 * mirrorCamera.right);
-    //    newLook = lookTarget;
-    //}
-    //void Update()
-    //{
-    //    newLook.x = lookTarget.x - (player.position.x - mirrorCamera.position.x);
-    //    mirrorCamera.LookAt(newLook, Vector3.up);
-
-    //    dist = Vector3.Distance(player.transform.position, mirrorCamera.transform.position);
-    //    mirrorCamera.GetComponent<Camera>().fieldOfView = 80 - (dist * 2);
-    //}
 }
--- Reflectrans/Assets/Yuya/Script/mirrorTest.cs
+++ Reflectrans/Assets/Yuya/Script/mirrorTest.cs
@@ -10,14 +10,38 @@ public class mirrorTest : MonoBehaviour
 
     void Update()
     {
+        Vector3 targetDir = targetPlayer.position - transform.position;
+
+        //playerDirection.transform.rotation = new Quaternion(0, targetDir.y, 0, 0);
+
+        //float angle = Vector3.Angle(targetDir, transform.forward);
+
+        //playerDirection.transform.rotation = Quaternion.Euler(0, angle, 0);
+        //print(playerDirection.transform.rotation);
+
+
+
+
+        //float targetDirX = targetPlayer.position.x - transform.position.x;
+        //float targetDirY = targetPlayer.position.y - transform.position.y;
+
+
+        //float radian = Mathf.Atan2(targetDirX, targetDirY);
+        //float degree = radian * Mathf.Rad2Deg;
+        //lineTransform.eulerAngles = new Vector3(0, 0, degree);             
+
+
+
         playerDirection.transform.LookAt(targetPlayer);
-        print(playerDirection.transform.rotation);
+        //print(playerDirection.transform.rotation);
 
         cameraDirection.transform.rotation = new Quaternion(
             0, playerDirection.transform.rotation.y * -1, 0, playerDirection.transform.rotation.w);
 
-        //playerDirection.transform.rotation = new Quaternion(
-        //    0, Mathf.Clamp(playerDirection.transform.rotation.y, -90, 90), 0, playerDirection.transform.rotation.w);
+        playerDirection.transform.rotation = new Quaternion(
+            0, Mathf.Clamp(playerDirection.transform.rotation.y, -90, 90), 0, playerDirection.transform.rotation.w);
+
+
 
 
         //var aim = this.targetPlayer.transform.position - transform.position;
